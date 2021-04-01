2b1a3890aea1894e9010d5daace6180753391d2a
Andremarcucci98 <marcuccimaciel.andre@gmail.com>
2021-04-01 12:30:07 -0300

    Iniciando estudos

---
+++ numerosalgunscuidados.js
@@ -0,0 +1,7 @@
+console.log(7 / 0.0001)
+console.log('10' / 2) // funciona
+console.log('3' + 2) // String ganha
+console.log('Show!' * 2) //Not a number
+console.log(0.1 + 0.7)
+//console.log(10.toString())
+console.log((10).toFixed(2))
